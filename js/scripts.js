48dfcdbe51af06727b595e309a123500d768d961
RGB Studios <justingolden21@gmail.com>
2019-03-09 01:21:51 -0800

    updated file structure, fixed links
    also made default background color on columns light blue

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+$(function() {
+	$('#col-security').hover(function() {
+		$('#lock-open-icon').css('display', 'none');
+		$('#lock-closed-icon').css('display', 'inline');
+	});
+	$('#col-security').mouseleave(function() {
+		if(! $(this).hasClass('active') ) {
+			$('#lock-open-icon').css('display', 'inline');
+			$('#lock-closed-icon').css('display', 'none');			
+		}
+	});
+
+
+	$('#col-infra').hover(function() {
+		$('.extra-server-icon').css('display', 'inline');
+	});
+	$('#col-infra').mouseleave(function() {
+		if(! $(this).hasClass('active') ) {
+			$('.extra-server-icon').css('display', 'none');
+		}
+	});
+
+
+	$('#slider').slick({
+		// dots: true
+		autoplay: true,
+		autoplaySpeed: 3000,
+		speed: 750
+	});
+
+	$('#slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		$('.big-col-inner').removeClass('active');
+		$('#col-'+ 'cloud security infra'.split(' ')[nextSlide]).addClass('active');
+		if(nextSlide==1) {
+			$('#lock-open-icon').css('display', 'none');
+			$('#lock-closed-icon').css('display', 'inline');
+		} else {
+			if(! $('#col-security').is(':hover') ) {
+				$('#lock-open-icon').css('display', 'inline');
+				$('#lock-closed-icon').css('display', 'none');
+			}
+		}
+		if(nextSlide==2) {
+			$('.extra-server-icon').css('display', 'inline');
+		} else {
+			if(! $('#col-infra').is(':hover') ) {
+				$('.extra-server-icon').css('display', 'none');
+			}
+		}
+		// console.log(nextSlide);
+	});
+
+	$('#col-cloud').click(function() {
+		$('#slider').slick('goTo', 0);
+	});
+	$('#col-security').click(function() {
+		$('#slider').slick('goTo', 1);
+	});
+	$('#col-infra').click(function() {
+		$('#slider').slick('goTo', 2);
+	});
+
+	$('#col-cloud').addClass('active');
+	
+
+});
