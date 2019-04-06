8953e004666de2ba32a6e4933ab6f94cf38a3492
Justin Golden <justingolden21@gmail.com>
2019-04-06 14:58:00 -0700

    many minor changes (see desc)
    - head.html: added fb metadata edited from old website
    
    - common-styles.css: added common styles for icons and links
    
    - address-links.js: auto-generated links now retain origonal id
    
    - contact.html: add required attribute to required form inputs, change button to submit button, add a elements with hrefs to icons
    
    - todo.txt: note to remember to purchase rights to fontawesome or change icon set to material icons
    
    -

--- assets/js/address-links.js
+++ assets/js/address-links.js
@@ -1,8 +1,8 @@
 $(function() {
 	// convert address tags to google maps links - made by Michael Jasper
 	$('p.address').each(function() {
-		let link = '<a href=""https://maps.google.com/maps?q=' + encodeURIComponent($(this).text() ) + '" target="_blank">' + $(this).text() + '</a>';
+		let link = '<a id="' + $(this).attr('id') +  '" href="https://maps.google.com/maps?q=' + encodeURIComponent($(this).text() ) + '" target="_blank">' + $(this).text() + '</a>';
 		$(this).html(link);
-		console.log(link);
+		// console.log(link);
 	});
 });
