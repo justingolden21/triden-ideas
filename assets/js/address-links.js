bdf3db7c1c02c9a63a431f8bb215968941607c41
Justin Golden <justingolden21@gmail.com>
2019-04-09 02:44:54 -0700

    fixed indentation

--- assets/js/address-links.js
+++ assets/js/address-links.js
@@ -1,7 +1,7 @@
 $(function() {
 	// convert address tags to google maps links - made by Michael Jasper
 	$('p.address').each(function() {
-		let link = '<a id="' + $(this).attr('id') +  '" href="https://maps.google.com/maps?q=' + encodeURIComponent($(this).text() ) + '" target="_blank">' + $(this).text() + '</a>';
+		let link = '<a id="' + $(this).attr('id') + '" href="https://maps.google.com/maps?q=' + encodeURIComponent($(this).text() ) + '" target="_blank">' + $(this).text() + '</a>';
 		$(this).html(link);
 		// console.log(link);
 	});
