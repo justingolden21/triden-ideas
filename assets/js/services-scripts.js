211370dff053b53a0c251e214f430e7450aa7e3b
Justin Golden <justingolden21@gmail.com>
2019-09-17 14:57:39 -0700

    several changes as discussed in previous meeting
    - image on every other side
    
    -services made third children into second children
    
    - moved caret on same line, has all open in IE/Edge
    
    - fixed up scaling of undraw images
    
    - homepage margin 0 above #about-us-div

--- assets/js/services-scripts.js
+++ assets/js/services-scripts.js
@@ -1,5 +1,11 @@
 $(function() {
 	$('details').click(function(evt) {
 		$('details').not(this).removeAttr('open');
+		$('.fa-caret-down').addClass('fa-caret-right').removeClass('fa-caret-down');
+
+		if(! $(this).prop('open') )
+			$(this).find('.fa-caret-right').addClass('fa-caret-down').removeClass('fa-caret-right');
+		else
+			$(this).find('.fa-caret-down').addClass('fa-caret-right').removeClass('fa-caret-down');
 	});
 });
